aecf670edfa37215cd816e3c3bbfb2110a70ff63
Nur Islam <nurislam0333@gmail.com>
2018-01-20 17:53:44 +0600

    Test | get data from backend & display it on the forntend

--- projectName/client/src/App.js
+++ projectName/client/src/App.js
@@ -3,19 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            users: []
+        }
+    }
+
+    componentDidMount = () => {
+        fetch('/users')
+        .then(res => res.json())
+        .then(users => this.setState({
+            users: users
+        }))
+    }
+
+    render() {
+        const users = this.state.users;
+        return (
+            <div className="App">
+                <h1>Users</h1>
+                {users.map((user, id) =>
+                    <div key={id}>{user.firstName} {user.lastName}</div>
+                )}
+            </div>
+        );
+    }
 }
 
 export default App;
--- projectName/routes/users.js
+++ projectName/routes/users.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json([
+      {id: 1, firstName: "nur", lastName: "islam"},
+      {id: 2, firstName: "nur1", lastName: "islam2"},
+      {id: 3, firstName: "nur3", lastName: "islam3"}
+  ]);
 });
 
 module.exports = router;
